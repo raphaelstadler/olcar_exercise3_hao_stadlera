637fba8ed8501b928fea5c0b98c3cf67964eb1de
raphaelstadler <stadlerraphael@gmx.ch>
2015-05-25 10:38:15 +0200

    Revert "Fixed some errors &made it more compact :haircut:"
    This reverts commit 5ab4cf768100a285ec74b961268765fb1bc3494d.

--- code/PI2_Update.m
+++ code/PI2_Update.m
@@ -53,7 +53,7 @@ function delta_theta = PI2_Update(Task,batch_sim_out,batch_cost)
 n_u  = size(batch_sim_out(1).u,1);
     
 % The total number of parameters per input
-n_theta = size(batch_sim_out(1).Controller.theta,1); % (n+1)*numstates: will never be used
+n_theta = size(batch_sim_out(1).Controller.theta,1); % (n+1)*numstates
     
 % The length of the Upsilone ("Y") basis function (time-varying basis funciton)
 n_gaussian = Task.n_gaussian;
@@ -78,7 +78,7 @@ R = batch_cost; % size: n_rollouts x n_time
 % adjust the lambda scaling parameter
 minR = repmat( min(R,[],1), [n_rollouts 1]);
 maxR = repmat( max(R,[],1), [n_rollouts 1]);
-medR = repmat( median(R,1), [n_rollouts 1]);    % will never be used
+medR = repmat( median(R,1), [n_rollouts 1]);
 
 % \exp(-1/lambda R)
 expR = exp(-10*(R-minR)./(maxR-minR));
@@ -86,31 +86,29 @@ alpha = expR./repmat( sum(expR,1), [n_rollouts 1]);
 
 % Used for intermediate calculations to finally come to delta_theta_i
 delta_theta_i_s = zeros(n_gaussian, n_states+1, n_time);
-
+% Same but not dependent on time.
+delta_theta_i = zeros(n_gaussian, n_states+1);
 % Also initialize complete delta_theta
 delta_theta = zeros(n_gaussian*(n_states+1),n_u);
-
 %%
-x_1 = batch_sim_out(1).x;
-t_1 = batch_sim_out(1).t;
-
 for i = 1:n_u % for each input i
     for s = 1:n_time % for each time
-        
-        % Calculate Y(s) here; Y(s) doesn't change throughout the rollouts.
-        upsilonBarVec = batch_sim_out(1).Controller.BaseFnc(t_1(s), x_1); % BaseFnc(t, x) % (n+1)*g x 1
-        upsilonBarMat = vec2mat(upsilonBarVec); % g x (n+1)
-        
-        upsilonVec = upsilonBarMat(:,1); % Y(s): g x 1
-        upsilonFactorMatrix = (upsilonVec*upsilonVec')/(upsilonVec'*upsilonVec);
         
         % Calculate the time varying parameter increment delta_theta_i_s
         sumDeltaTheta = zeros(n_gaussian,n_states+1);
         for k = 1:n_rollouts   
-            epsilonVec = batch_sim_out(k).eps(:,i,s);   % k-th epsilon
+            x_k = batch_sim_out(k).x;
+            t_k = batch_sim_out(k).t;
+            
+            epsilonVec = batch_sim_out(k).eps(:,i,s);
             epsilonMat = vec2mat(epsilonVec);
-
-            upsEpsMatrix = upsilonFactorMatrix * epsilonMat; % (g x g) * (g x (n + 1))
+            
+            upsilonBarVec = batch_sim_out(k).Controller.BaseFnc(t_k(s), x_k); % BaseFnc(t, x) % (n+1)*g x 1
+            upsilonBarMat = vec2mat(upsilonBarVec); % g x (n+1)
+            upsilonVec = upsilonBarMat(:,1); % g x 1
+            
+            
+            upsEpsMatrix = (upsilonVec*upsilonVec')/(upsilonVec'*upsilonVec) * epsilonMat; % (g x g) * (g x (n + 1))
  
             sumDeltaTheta = sumDeltaTheta + alpha(k,s)*upsEpsMatrix; % scalar * matrix
         end
@@ -119,17 +117,41 @@ for i = 1:n_u % for each input i
     end % for..s
     
     % Time-averaging the parameter vector
-    upsilonBarVecCollection = batch_sim_out(1).Controller.BaseFnc(t_1, x_1); % BaseFnc(t, x) % (n+1)*g x t
-    upsilonBarMatCollection = vec2mat(upsilonBarVecCollection); % g x (n+1) x t
-    
-    upsilonVecCollection = upsilonBarMatCollection(:,1,:); % Y(s): g x 1 x t
-    upsilonMatCollection = repmat(upsilonVecCollection, [1 (n_states+1) 1]); % g x (n+1) x t
+    x_1 = batch_sim_out(1).x;
+    t_1 = batch_sim_out(1).t;
+    upsilonBarVec = batch_sim_out(1).Controller.BaseFnc(t_1, x_1); % BaseFnc(t, x) % g*(n+1) x t
+    upsilonBarMat = vec2mat(upsilonBarVec); % g x (n+1) x t
 
-    delta_theta_i = sum((upsilonMatCollection .* delta_theta_i_s),3) ./ sum(upsilonMatCollection,3); % g x (n+1)
+    delta_theta_i = sum((upsilonBarMat .* delta_theta_i_s),3) ./ sum(upsilonBarMat,3);
+    
+%     for j = 1:size(delta_theta_i_s,2) % For the jth column of delta_theta_i_s matrix    
+%         x_1 = batch_sim_out(1).x;
+%         t_1 = batch_sim_out(1).t;
+%         
+%         % Time-averaging the parameter vector
+%         
+%         % Calculate integrand to assign delta_theta_i_s
+%         integrandDeltaTheta = zeros(n_gaussian,1);
+%         normalizationVector = zeros(n_gaussian,1);
+%         
+%         for s = 1:n_time
+%             upsilonBarVec = batch_sim_out(1).Controller.BaseFnc(t_1(s), x_1); % BaseFnc(t, x)
+%             upsilonBarMat = vec2mat(upsilonBarVec); % g x (n+1)
+% 
+%             upsilonVec_S = upsilonBarMat(:,1); % g x 1
+%             
+%             normalizationVector = normalizationVector + upsilonVec_S;
+%         
+%             integrandDeltaTheta = integrandDeltaTheta + delta_theta_i_s(:,j,s) .* upsilonVec_S;  
+%         end
+%         
+%         delta_theta_i(:,j) = integrandDeltaTheta ./ normalizationVector;        
+%     end % for..j
 
     %% conversion back to vector-style
     delta_theta(:,i) = mat2vec(delta_theta_i);
 end
+
 
 %% additional functions vec2mat and mat2vec that are provided
 function mat = vec2mat(vec)
@@ -148,5 +170,6 @@ function vec = mat2vec(mat)
     mat = permute(mat, [2 1 3]);
     vec = reshape(mat,[],1,size(mat,3));
 end
+
 
 end
